ec71597015ac9fcb7f44ad9f8788b2872e436eb2
Hrach Tadevosyan <tadevosyanhrach@gmail.com>
2023-12-21 23:38:57 -0800

    add logic for password check middleware

--- app/Http/Middleware/HasVeriefiedPassword.php
+++ app/Http/Middleware/HasVeriefiedPassword.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cookie;
 use Symfony\Component\HttpFoundation\Response;
 
 class HasVeriefiedPassword
@@ -15,6 +16,9 @@ class HasVeriefiedPassword
      */
     public function handle(Request $request, Closure $next): Response
     {
+        if (! Cookie::has('password_step_passed') || Cookie::get('password_step_passed') !== 'true') {
+            return redirect()->to('/');
+        }
         return $next($request);
     }
 }
